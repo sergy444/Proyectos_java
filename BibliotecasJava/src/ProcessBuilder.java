455225e44194d56e128d621e9be5e92dcd86b107
Sonia Ortiz de Arri <soniaodea@gmail.com>
2014-10-28 11:53:15 +0100

    .

--- BibliotecasJava/src/ProcessBuilder.java
+++ BibliotecasJava/src/ProcessBuilder.java
@@ -2,7 +2,7 @@ import java.lang.*;
 class ProcessBuilder {
     public static void main( String ... args ) throws java.io.IOException , InterruptedException {
         System.out.println(" Este es Java ");
-        new ProcessBuilder("./Lp.sh" ).start().waitFor();
+        new ProcessBuilder("./algo.cmd" ).start().waitFor();
         System.out.println("Se acabó la ejecución" );
     }
 }
